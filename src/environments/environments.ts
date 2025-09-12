fdd1dfe785e9999390bd0e44a2d0d8f22766ffd1
Amal Sigi <amalsiji4764@gmail.com>
2025-09-12 15:42:22 +0530

    new update

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //  api: 'https://api.zdesire.com/api',
-    api: 'http://localhost:5224/api',
+  // api: 'https://api.zdesire.com/api',
+  api: 'http://localhost:5000/api',
 };
